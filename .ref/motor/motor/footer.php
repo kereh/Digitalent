3b5b5c39f771a3822400b221f0a0d6a0a9c1c079
ronaldo <kereh>
2024-08-28 22:14:00 +0800

    add .gitignore

--- .ref/motor/motor/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Footer -->
-<footer class="w3-center w3-black w3-padding-16">
-  <p>Powered by <a href="#" title="W3.CSS" target="_blank" class="w3-hover-text-green">Komunitas Club Motor</a></p>
-</footer>
--- .ref/motor/motor/head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Komunitas Club Motor</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/w3.css">
-</head>
-<body>
