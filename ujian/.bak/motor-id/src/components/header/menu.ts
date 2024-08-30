773966c980ece14b30318d830994714e8c2bf6fd
ronaldo <kereh>
2024-08-30 10:27:33 +0800

    add some shit

--- ujian/.bak/motor-id/src/components/header/menu.ts
+++
@@ -1,7 +0,0 @@
-export const menus = [
-  { text: "Home", url: "/" },
-  { text: "Products", url: "/products" },
-  { text: "Gallery", url: "/gallery" },
-  { text: "Article & Event", url: "/event" },
-  { text: "About", url: "/about" },
-];
--- ujian/.bak/motor-id/src/components/theme/theme-provider.tsx
+++
@@ -1,7 +0,0 @@
-import { ThemeProvider as NextTheme } from "next-themes"
-import type { ThemeProviderProps } from "next-themes/dist/types"
-import {} from "lucide-react"
-
-export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextTheme {...props}>{children}</NextTheme>
-}
